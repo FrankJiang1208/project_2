2c8454fc103e659445a818571d35c3eefd4083f6
su <adsu@uci.edu>
2021-09-08 14:54:34 -0700

    updating

--- joinqueries.sql
+++ joinqueries.sql
@@ -1,6 +1,7 @@
 select * from artists;
 select * from billboard;
 select * from grammy_songs;
+select * from grammy_songs_joined_billboard;
 
 --rank by genre, number of times it appears in data
 select b.genre, count(b.genre) from billboard b group by b.genre order by count(b.genre) DESC
@@ -13,34 +14,24 @@ select g.artist, count(g.artist) from grammy_songs g group by g.artist order by 
 
 ----------------------------------------------------------------------------------------------------
 --join Artists and Billboard data
-select b.artist, b.year , b.weeks_on_chart,   a.peak_position, a.num_albums
+select b.artist, b.name_of_song, b.year , b.weeks_on_chart,   b.peak_position, a.num_albums, a.genre
 from billboard b
 inner join artists a
 on  a.artist = b.artist
 
 --all join (raw)
-select b.artist, b.week, b.weeks_on_chart,   a.genres, a.num_albums, g.grammy_award, g.grammy_year
+select b.artist, b.year, b.weeks_on_chart,   a.genre, a.num_albums, g.grammy_award, g.grammy_year
 from billboard b
 inner join artists a
 on  a.artist = b.artist
 left join grammy_songs g
 on b.artist = g.artist
 
-
---deleting duplicate rows due to weekly_rank and making an edited table (test)
-select * into test from billboard 
-delete from test a using (select min(ctid) as ctid, artist from test group by artist having count(*)>1) b where a.artist = b.artist and a.ctid <> b.ctid
-select * from test
---all join (shows which billboard songs have grammy awards)
-select a.artist, t.week, t.weeks_on_chart,   a.genres, a.num_albums, g.grammy_award, g.grammy_year
-from test t
-inner join artists a
-on  a.artist = t.artists
-left join grammy_songs g
-on t.artist = g.artist
-order by g.grammy_year asc
---join showing which grammy songs have billboard ranks
-select g.artist, g.artist, g.grammy_award, g.grammy_year, t.peak_position
+--all join
+select g.artist, g.name_of_song, g.grammy_award, g.grammy_year, b.peak_position, a.genre
 from grammy_songs g	
-left join test t
-on g.artist = t.artist
+left join billboard b
+on g.name_of_song = b.name_of_song
+inner join artists a
+on g.artist = a.artist
+--order by b.peak_position asc
